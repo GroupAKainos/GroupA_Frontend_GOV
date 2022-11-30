c4479f40a402c874cff5103605613d2b026de12a
Mihnea Boca <mihnea.boca@kainos.com>
2022-11-30 15:16:56 +0000

    Added registration page

--- app/controller/JobController.js
+++ app/controller/JobController.js
@@ -2,18 +2,30 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-const employee = require('../service/JobService')
+const job = require('../service/JobService')
+
+const userData = require('../service/UserService')
 
 router.get('/viewroles',  async (req, res) =>  {
-    let s = await employee.viewjobroles()
+    let s = await job.viewjobroles()
     res.render('viewroles', { roles: s })
 })
 
 router.get('/viewcompetencies/:BandID',  async (req, res) =>  {
     let bandID = req.params.BandID
-    let s = await employee.viewcompetency(bandID)
+    let s = await job.viewcompetency(bandID)
 
     res.render('competenciesperband', { comps: s, band: bandID })
 })
+
+router.get('/register', function(req, res){ 
+    res.render('register'); 
+});
+
+router.post('/register', async (req, res)=>{ 
+    var user = req.body 
+    let insertedKey = await userData.registerUser(user.email, user.password, user.role)
+    res.render('register'); 
+});
 
 module.exports = router
---
+++ app/service/UserService.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const url = process.env.URL
+
+exports.registerUser = async (email, password, role) => { 
+    const response = await axios.post(url+'/register', { "email": email, "password": password, "role": role })
+    return response
+}
