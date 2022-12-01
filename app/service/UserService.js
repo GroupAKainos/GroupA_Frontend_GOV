2376c93140b5e40258f4c5f072a2e9feaacf42e6
Mihnea Boca <mihnea.boca@kainos.com>
2022-12-01 14:23:23 +0000

    Added tests

--- app/service/UserService.js
+++ app/service/UserService.js
@@ -2,6 +2,14 @@ const axios = require('axios');
 const url = process.env.URL
 
 exports.registerUser = async (email, password, role) => { 
-    const response = await axios.post(url+'/register', { "email": email, "password": password, "role": role })
-    return response.data
+    try {
+        const response = await axios.post(url+'/register', { "email": email, "password": password, "role": role })
+        return response.data
+    } catch (e) {
+        if((e+'').includes('Request failed with status code 500'))
+            return new Error('Could not register user')
+        if((e+'').includes('Request failed with status code 400'))
+            return new Error('Invalid data')
+        return new Error('Error')
+    }
 }
---
+++ test/Unit/Service/UserServiceTest.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const chai = require("chai");
+const expect = chai.expect;
+const EmployeeService = require ('../../../app/service/UserService');
+const url = process.env.URL
+
+const user ={
+    email:"Something@anything.com",
+    password:"Password!",
+    role:"Admin"
+}
+
+describe('UserService', function () {
+    describe('registerUser', function () {
+        it('should return error 500', async () => {
+            var mock = new MockAdapter(axios);
+
+            mock.onPost(url+"/register").reply(500);
+
+            var error = await EmployeeService.registerUser(user.email, user.password, user.role)
+        
+            expect(error.message).to.equal('Could not register user')
+        })
+        it('should return error 400', async () => {
+            var mock = new MockAdapter(axios);
+
+            mock.onPost(url+"/register").reply(400);
+
+            var error = await EmployeeService.registerUser(user.email, user.password, user.role)
+        
+            expect(error.message).to.equal('Invalid data')
+        })
+    })
+})
