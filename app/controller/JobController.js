8fc8b408f026bea2833d79adcd07a6cdce3e3d58
Ryan-Jones1999 <ryan.jones@kainos.com>
2022-12-02 15:58:24 +0000

    rebased with main and changed controller class to use correct variable

--- app/controller/JobController.js
+++ app/controller/JobController.js
@@ -17,9 +17,9 @@ router.get('/viewcompetencies/:BandID',  async (req, res) =>  {
 })
 
 router.get('/addnewjob',  async (req, res) =>  {
-    let family = await employee.populatefamilylist()
-    let capability = await employee.poulatecapabiltynamelist()
-    let bandlevel = await employee.poulatebandlevellist()
+    let family = await job.populatefamilylist()
+    let capability = await job.poulatecapabiltynamelist()
+    let bandlevel = await job.poulatebandlevellist()
 
    res.render('addnewjob', { family: family, capability:capability, bandlevel: bandlevel})
 })
@@ -27,40 +27,40 @@ router.get('/addnewjob',  async (req, res) =>  {
 router.post('/addnewjob', async (req, res) => { 
     //Frontend validation - returns the req body and list so user doesnt lose any information they had typed if the check fails
     if(req.body.jobName.length < 5 ){
-        let family = await employee.populatefamilylist()
-        let capability = await employee.poulatecapabiltynamelist()
-        let bandlevel = await employee.poulatebandlevellist()
+        let family = await job.populatefamilylist()
+        let capability = await job.poulatecapabiltynamelist()
+        let bandlevel = await job.poulatebandlevellist()
 
         res.locals.errormessage = "Jobname length is under 5 characters role has not been created"
         res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
 
     }else if(req.body.jobResponsibility.length < 10){
-        let family = await employee.populatefamilylist()
-        let capability = await employee.poulatecapabiltynamelist()
-        let bandlevel = await employee.poulatebandlevellist()
+        let family = await job.populatefamilylist()
+        let capability = await job.poulatecapabiltynamelist()
+        let bandlevel = await job.poulatebandlevellist()
 
         res.locals.errormessage = "Job Responsibility length is under 10 characters role has not been created"
         res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
     }else if( req.body.specSummary.length < 8){
-        let family = await employee.populatefamilylist()
-        let capability = await employee.poulatecapabiltynamelist()
-        let bandlevel = await employee.poulatebandlevellist()
+        let family = await job.populatefamilylist()
+        let capability = await job.poulatecapabiltynamelist()
+        let bandlevel = await job.poulatebandlevellist()
 
         res.locals.errormessage = "Specification summary length is under 8 characters role has not been created"
         res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
     }else{
-        let addrole = await employee.addnewrole(req.body)
+        let addrole = await job.addnewrole(req.body)
         
         // If the value returned from the job service is not equal to an error render the view roles page with added role and message to user
         // Else populate lists return the data entered and render the addnewroles page with message to let the user know
         if(!(addrole instanceof Error)){
         let message = "New role has been added"
-        let s = await employee.viewjobroles()
+        let s = await job.viewjobroles()
         res.render('viewroles', { roles: s, newrolesuccess: message})
         }else{
-        let family = await employee.populatefamilylist()
-        let capability = await employee.poulatecapabiltynamelist()
-        let bandlevel = await employee.poulatebandlevellist()
+        let family = await job.populatefamilylist()
+        let capability = await job.poulatecapabiltynamelist()
+        let bandlevel = await job.poulatebandlevellist()
 
         res.locals.errormessage = "Theres been an error adding new role please try again "
         res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
