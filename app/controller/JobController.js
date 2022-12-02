40a180ba8986ad8414a3a54919cf96fa1e679b8d
Ryan-Jones1999 <ryan.jones@kainos.com>
2022-12-02 15:51:11 +0000

    Updated page name to make it more consistent and added gitignore to stop tracking .idea/

--- app/controller/JobController.js
+++ app/controller/JobController.js
@@ -21,7 +21,7 @@ router.get('/addnewjob',  async (req, res) =>  {
     let capability = await employee.poulatecapabiltynamelist()
     let bandlevel = await employee.poulatebandlevellist()
 
-   res.render('addnewrole', { family: family, capability:capability, bandlevel: bandlevel})
+   res.render('addnewjob', { family: family, capability:capability, bandlevel: bandlevel})
 })
 
 router.post('/addnewjob', async (req, res) => { 
@@ -32,7 +32,7 @@ router.post('/addnewjob', async (req, res) => {
         let bandlevel = await employee.poulatebandlevellist()
 
         res.locals.errormessage = "Jobname length is under 5 characters role has not been created"
-        res.render('addnewrole', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
+        res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
 
     }else if(req.body.jobResponsibility.length < 10){
         let family = await employee.populatefamilylist()
@@ -40,14 +40,14 @@ router.post('/addnewjob', async (req, res) => {
         let bandlevel = await employee.poulatebandlevellist()
 
         res.locals.errormessage = "Job Responsibility length is under 10 characters role has not been created"
-        res.render('addnewrole', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
+        res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
     }else if( req.body.specSummary.length < 8){
         let family = await employee.populatefamilylist()
         let capability = await employee.poulatecapabiltynamelist()
         let bandlevel = await employee.poulatebandlevellist()
 
         res.locals.errormessage = "Specification summary length is under 8 characters role has not been created"
-        res.render('addnewrole', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
+        res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
     }else{
         let addrole = await employee.addnewrole(req.body)
         
@@ -63,7 +63,7 @@ router.post('/addnewjob', async (req, res) => {
         let bandlevel = await employee.poulatebandlevellist()
 
         res.locals.errormessage = "Theres been an error adding new role please try again "
-        res.render('addnewrole', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
+        res.render('addnewjob', { req: req.body ,family: family, capability:capability, bandlevel: bandlevel})
         }
     }
 })
